f502622bab247ab58b1497c6a19cd289c77f3b77
Patrick <PatrickAnnik02@gmail.com>
2025-07-28 19:25:15 -0300

    feat: add Task class and Status enum

---
+++ src/main/java/com/patrickannik02/tasktracker/Status.java
@@ -0,0 +1,7 @@
+package com.patrickannik02.tasktracker;
+
+public enum Status {
+    DONE,
+    IN_PROGRESS,
+    TODO
+}
---
+++ src/main/java/com/patrickannik02/tasktracker/Task.java
@@ -0,0 +1,63 @@
+package com.patrickannik02.tasktracker;
+
+import java.time.LocalDate;
+
+public class Task {
+
+    private final int id;
+    private String description;
+    private Status status;
+    private final LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    public Task(int id, String description, Status status, LocalDate createdAt, LocalDate updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+}
