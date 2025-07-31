c9af50ca1635a18bfda46a1cc3898010e7a14322
Patrick <PatrickAnnik02@gmail.com>
2025-07-31 14:57:32 -0300

    fix: correct some implementations of Task class

--- src/main/java/com/patrickannik02/tasktracker/Task.java
+++ src/main/java/com/patrickannik02/tasktracker/Task.java
@@ -10,15 +10,30 @@ public class Task {
     private final LocalDate createdAt;
     private LocalDate updatedAt;
 
+    // Constructor para tareas nuevas
     public Task(String description) {
+        this.id = 0;
         this.description = description;
         this.status = Status.TODO;
         this.createdAt = LocalDate.now();
         this.updatedAt = LocalDate.now();
     }
 
+    // Constructor para tareas existentes
+    public Task(int id, String description, Status status,  LocalDate createdAt, LocalDate updatedAt) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getDescription() {
