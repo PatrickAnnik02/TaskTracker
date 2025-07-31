58c8f143473fe4c0a0935af1f052a7d959c98df4
Patrick <PatrickAnnik02@gmail.com>
2025-07-31 13:47:12 -0300

    feat: add methods to the interface ITaskRepository

--- src/main/java/com/patrickannik02/tasktracker/ITaskRepository.java
+++ src/main/java/com/patrickannik02/tasktracker/ITaskRepository.java
@@ -1,6 +1,14 @@
 package com.patrickannik02.tasktracker;
 
-public class ITaskRepository {
+import java.util.List;
 
+public interface ITaskRepository {
 
+    public void save(Task task);
+
+    public void delete(Task task);
+
+    public List<Task> findAll();
+
+    public List<Task> findAllByStatus(Status status);
 }
