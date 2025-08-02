aebdbf410d1110c3cf8f78157f1f4cafbed7f01b
Patrick <PatrickAnnik02@gmail.com>
2025-08-02 14:36:18 -0300

    feat: begin implementation of TaskTracker main class

--- src/main/java/com/patrickannik02/tasktracker/TaskTracker.java
+++ src/main/java/com/patrickannik02/tasktracker/TaskTracker.java
@@ -2,8 +2,8 @@ package com.patrickannik02.tasktracker;
 
 public class TaskTracker {
     public static void main(String[] args) {
-
-
-
+        InMemoryTaskRepository taskRepository = new InMemoryTaskRepository();
+        TaskService taskService = new TaskService(taskRepository);
+        CommandProcessor commandProcessor = new CommandProcessor(taskService);
     }
 }
