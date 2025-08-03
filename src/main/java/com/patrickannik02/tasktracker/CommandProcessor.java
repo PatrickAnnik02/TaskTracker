ff782d4cf1dc09d1505df9bdb20cb4f5406daebc
Patrick <PatrickAnnik02@gmail.com>
2025-08-03 19:54:31 -0300

    fix: add some features to TaskTracker and CommandProcessor

--- src/main/java/com/patrickannik02/tasktracker/CommandProcessor.java
+++ src/main/java/com/patrickannik02/tasktracker/CommandProcessor.java
@@ -1,5 +1,8 @@
 package com.patrickannik02.tasktracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandProcessor {
     private final TaskService taskService;
 
@@ -16,11 +19,35 @@ public class CommandProcessor {
             throw new IllegalArgumentException("task argument cannot be empty.");
         } else {
             switch (args[0]) {
+                case "exit":
+                    if (args.length != 1) {
+                        throw new IllegalArgumentException("cannot read the command");
+                    } else {
+                        return;
+                    }
+                case "help":
+                    if (args.length != 1) {
+                        throw new IllegalArgumentException("cannot read the command.");
+                    } else {
+                        System.out.println("usage: task-cli [option] [arg(s)]");
+                        System.out.println("add \"description\"                   - Add new task.");
+                        System.out.println("update <id> \"description\"           - Update task with specified id.");
+                        System.out.println("delete <id>                         - Delete task with specified id.");
+                        System.out.println("mark-todo <id>                      - Mark as to do task with specified id.");
+                        System.out.println("mark-in-progress <id>               - Mark as in progress task with specified id.");
+                        System.out.println("mark-done <id>                      - Mark as done task with specified id.");
+                        System.out.println("list                                - List all tasks.");
+                        System.out.println("list done                           - List all tasks done.");
+                        System.out.println("list todo                           - List all tasks to do.");
+                        System.out.println("list in-progress                    - List all tasks done.");
+                    }
+                    break;
                 case "add":
                     if (args.length != 2) {
                         throw new IllegalArgumentException("cannot read the command.");
                     } else {
                         taskService.addTask(args[1]);
+                        System.out.println("Task '" + args[1] + "' added successfully.");
                     }
                     break;
                 case "delete":
@@ -29,6 +56,7 @@ public class CommandProcessor {
                     } else {
                         int id = Integer.parseInt(args[1]);
                         taskService.deleteTask(id);
+                        System.out.println("Task with ID " + id + " deleted successfully.");
                     }
                     break;
                 case "update":
@@ -57,17 +85,21 @@ public class CommandProcessor {
                     break;
                 case "list":
                     if (args.length == 1) {
-                        taskService.tasks();
+                        List<Task> allTasks = taskService.tasks();
+                        printTasks(allTasks);
                     } else if (args.length == 2) {
                         switch (args[1]) {
                             case "done":
-                                taskService.doneTasks();
+                                List<Task> doneTasks = taskService.doneTasks();
+                                printTasks(doneTasks);
                                 break;
                             case "in-progress":
-                                taskService.inProgressTasks();
+                                List<Task> inProgressTasks = taskService.inProgressTasks();
+                                printTasks(inProgressTasks);
                                 break;
                             case "todo":
-                                taskService.pendingTasks();
+                                List<Task> todoTasks = taskService.pendingTasks();
+                                printTasks(todoTasks);
                                 break;
                         }
                     } else {
@@ -75,5 +107,17 @@ public class CommandProcessor {
                     }
             }
         }
+    }
+
+    private void printTasks(List<Task> tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println("There are not any tasks to show.");
+            return;
+        }
+        System.out.println("\n--- Task list ---");
+        for (Task task : tasks) {
+            System.out.println(task.toString());
+        }
+        System.out.println("-----------------------\n");
     }
 }
--- src/main/java/com/patrickannik02/tasktracker/TaskTracker.java
+++ src/main/java/com/patrickannik02/tasktracker/TaskTracker.java
@@ -1,9 +1,15 @@
 package com.patrickannik02.tasktracker;
 
+import java.util.Scanner;
+
 public class TaskTracker {
     public static void main(String[] args) {
         InMemoryTaskRepository taskRepository = new InMemoryTaskRepository();
         TaskService taskService = new TaskService(taskRepository);
         CommandProcessor commandProcessor = new CommandProcessor(taskService);
+
+        System.out.println("Welcome to TaskTracker.");
+        System.out.println("Write 'help' to commands or 'exit' to exit.");
+        commandProcessor.execute(args);
     }
 }
